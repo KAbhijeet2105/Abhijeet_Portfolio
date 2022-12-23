65e0915cd1de66eb1a3229ffd6b9d0526c396223
KAbhijeet2105 <ajayaish5@gmail.com>
2022-12-23 22:54:19 +0530

    added new sections : skills,work.

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 
-//import { AppWrap } from '../../wrapper';
+import { AppWrap } from '../../wrapper';
 import { images } from "../../constants";
 import "./Header.scss";
 
@@ -68,5 +68,5 @@ const Header = () => (
   </div>
 );
 
-export default Header;
-//export default AppWrap(Header, 'home');
+//export default Header;
+export default AppWrap(Header, 'home');
